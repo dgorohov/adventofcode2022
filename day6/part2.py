a82a8b7fa30229c3ce6c0a921a40eb2cc8b1e7ce
Dmitry Gorohov <dmitry.gorohov@gmail.com>
2022-12-06 07:19:16 +0100

    day6

---
+++ day6/part2.py
@@ -0,0 +1,18 @@
+from core import load_data
+
+
+def find_next_marker_pos(data, marker_len=4):
+    for packet in range(0, len(data), 1):
+        if len(set(data[packet:packet + marker_len])) == marker_len:
+            yield packet + marker_len
+
+
+def solution():
+    datastream = list(load_data("input.txt")[0])
+    # datastream = list("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+    packets = list(find_next_marker_pos(datastream, marker_len=14))
+    return packets[0]
+
+
+if __name__ == '__main__':
+    print(solution())
